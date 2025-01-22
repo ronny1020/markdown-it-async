3ad87dc732be83b0ff2cb1c0db7a233d9ccb2d69
Anthony Fu <github@antfu.me>
2025-01-22 19:48:22 +0800

    fix: try fix dts imports

--- build.config.ts
+++ build.config.ts
@@ -14,11 +14,18 @@ export default defineBuildConfig({
   hooks: {
     'build:done': async () => {
       for (const file of await glob('./dist/*.d.{cts,mts,ts}')) {
-        const content = await fs.readFile(file, 'utf-8')
+        let content = await fs.readFile(file, 'utf-8')
         // Override `options` type on dist dts only
-        const newContent = content.replace(
+        let newContent = content.replace(
           'class MarkdownItAsync extends MarkdownIt {',
           'class MarkdownItAsync extends MarkdownIt {\n     // @ts-ignore\n    options: MarkdownItAsyncOptions',
+        )
+        if (content === newContent)
+          throw new Error(`Failed to replace for ${file}`)
+        content = newContent
+        newContent = content.replace(
+          'import MarkdownIt',
+          'import type MarkdownIt',
         )
         if (content === newContent)
           throw new Error(`Failed to replace for ${file}`)
